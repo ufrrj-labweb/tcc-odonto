f85487262af2e5bde55c411ae098c257b9ae1420
Max Santos <max.santos@oliveiratrust.com.br>
2023-01-01 21:33:28 -0300

    design inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import QuestionarioView from '../views/QuestionarioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '',
+      name: 'questionario',
+      component: QuestionarioView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/questionario',
+    //   name: 'questionario',
+    //   component: QuestionarioView
+    // }
   ]
 })
 
---
+++ src/views/QuestionarioView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    
+    <h1 class="title has-text-centered is-1 mt-4">Odonto App</h1>
+    
+    <div class="box mb-6">
+      <h2 class="title has-text-centered">Título da pergunta</h2>
+
+      <p class="is-size-3 mb-5 has-text-justified">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, libero nec laoreet tempor, tellus eros tristique libero, sed sodales.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, libero nec laoreet tempor, tellus eros tristique libero, sed sodales.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, libero nec laoreet tempor, tellus eros tristique libero, sed sodales.
+      </p>
+
+      <div class="image-container">
+        <img class="image has-image-centered" src="https://www.designerd.com.br/wp-content/uploads/2020/04/imagem.png" alt="">
+      </div>
+
+      <div class="opcoes-container buttons are-medium mt-6 is-justify-content-center">
+        <button class="button">Opção A</button>
+        <button class="button">Opção B</button>
+        <button class="button">Opção C</button>
+        <button class="button">Opção D</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.box {
+  background-color: #FDFDFF;
+  padding: 20px 80px;
+}
+
+.button {
+  background-color: #E26D5A;
+  color: #FDFDFF
+}
+
+.button:hover {
+  background-color: #FDFDFF;
+  color: #E26D5A;
+  border-color: #E26D5A;
+}
+</style>
+
+
